2c08ce4272cd4f53cea36bfcfef3cb032d6cd68b
niming175 <niming175@qq.com>
2018-01-28 22:56:57 +0800

    提交myblog项目

---
+++ database/migrations/2018_01_25_145309_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id'); //increments 自增长
+            $table->string('slug')->uniqid(); // unique 唯一
+            $table->string('title');
+            $table->string('content');
+            $table->timestamps(); // 时间戳
+            $table->timestamp('published_at')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
